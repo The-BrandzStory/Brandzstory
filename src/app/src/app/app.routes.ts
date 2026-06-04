88ccbfcce1aec81c79812f37faa1a5c7a84ba9d5
bharanisivam <bharanisivam2@gmail.com>
2026-06-04 10:03:13 +0530

    remove nested

--- src/app/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LandingPage } from './components/landing-page/landing-page';
-import { Posters } from './components/posters/posters';
-
-export const routes: Routes = [
-    { path: '', component: LandingPage },
-    { path: 'posters', component: Posters }
-];
