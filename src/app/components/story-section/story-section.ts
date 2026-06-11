8cbf682610b101f4f4f67147c1e0f61eb42d8de0
TheBrandzStory <thebrandzstory@gmail.com>
2026-06-11 21:20:03 +0530

    video play update

--- src/app/components/story-section/story-section.ts
+++ src/app/components/story-section/story-section.ts
@@ -1,19 +1,34 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-story-section',
-  imports: [CommonModule],
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './story-section.html',
   styleUrl: './story-section.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StorySection {
 
-
   @Input() leftPills: { label: string; icon: string }[] = [];
   @Input() rightPills: { label: string; icon: string }[] = [];
 
+  @ViewChild('myVideo') videoRef!: ElementRef<HTMLVideoElement>;
 
+  playFullscreen(): void {
+    const video = this.videoRef.nativeElement;
+
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    }
+
+    video.play();
+  }
 }
